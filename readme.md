26cb95843e3071302526ec7643bedae8e3a5e304
Ilyas Serter <ilyasserter@gmail.com>
2015-08-22 13:55:56 +0300

    AddProduct

--- readme.md
+++ readme.md
@@ -2,6 +2,16 @@ A Laravel 5.1 package for TransUnion API
 ======
 This package provides a simplified facade to interact with the transunion API.
 
+Add the Service Provider
+---
+`Iserter\Transunion\ServiceProvider::class,`
+
+Add an alias to the service facade
+---
+`'Transunion' => Iserter\Transunion\Facades\Transunion::class,`
+
 Publish Config File
 ---
 `php artisan vendor:publish --provider="Iserter\Transunion\ServiceProvider" --tag=config`
+
+
--- src/TransunionService.php
+++ src/TransunionService.php
@@ -9,23 +9,55 @@ class TransunionService implements TransunionInterface {
     public function __construct(){
         $apiURL = config('transunion.testing') ? config('transunion.api_test_url') : config('transunion.api_url');
         $this->soapClient = new \SoapClient($apiURL,array('soap_version' => SOAP_1_2));
-
-        $this->setSoapHeaders();
     }
 
     public function getOperationList(){
         return $this->soapClient->__getFunctions();
     }
 
-    public function getApiVersion(){
-        return $this->soapClient->__soapCall('Version',[]);
+    public function getAuthHeader(){
+
+        $authParams = (object) ['Username' => config('transunion.api_username') , 'Password' => config('transunion.api_password')];
+
+        return new \SoapHeader('http://schemas.datacontract.org/2004/07/Tusa.Services.ConsumerConnect',
+                                'AuthenticationCredentials', $authParams);
     }
 
-    private function setSoapHeaders()
-    {
+    public function getApiVersion(){
         $headers = [];
         $headers[] = new \SoapHeader('http://www.w3.org/2005/08/addressing', 'Action', 'http://tempuri.org/IInDirect/Version');
-        $this->soapClient->__setSoapHeaders($headers);
+
+        try {
+            $response = $this->soapClient->__soapCall('Version', [],[],$headers);
+            return $response;
+        } catch(\SoapFault $faultResponse) {
+            throw new \Exception($faultResponse->detail->ArrayOfErrorDetails->ErrorDetails->ErrorMessage);
+        }
+    }
+
+    /**
+     * @param $customerDetail
+     * @param string $productCode
+     * @return mixed
+     * @throws \Exception
+     */
+    public function addProduct($customerDetail,$productCode = 'YCR001'){
+
+        $headers = [];
+        $headers[] = new \SoapHeader('http://www.w3.org/2005/08/addressing', 'Action', 'http://tempuri.org/IInDirect/AddProduct');
+        $headers[] = $this->getAuthHeader();
+        $params = ['CustomerDetail' => $customerDetail, 'ProductCode' => $productCode];
+
+        try {
+            $response = $this->soapClient->__soapCall('AddProduct', $params,[],$headers);
+            return $response;
+        } catch(\SoapFault $faultResponse) {
+            throw new \Exception($faultResponse->detail->ArrayOfErrorDetails->ErrorDetails->ErrorMessage);
+        }
+    }
+
+    public function getLastRequestHeaders(){
+        return $this->soapClient->__getLastRequestHeaders();
     }
 
 
